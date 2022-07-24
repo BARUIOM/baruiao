4910d35981d6f1411ce0492c0343abb95dff1bb6
Vitor Mac <5874907+listanegra@users.noreply.github.com>
2022-07-24 01:38:06 -0300

    Roboto & Material Icons font

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,15 @@
+import "@fontsource/roboto/100.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "@fontsource/roboto/900.css";
+
+import "@fontsource/material-icons";
+import "@fontsource/material-icons-outlined";
+
 import { createApp } from 'vue';
+
 import App from './App.vue';
 import './style.css';
 
