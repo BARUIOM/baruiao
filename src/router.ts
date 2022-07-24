92ae1f702ad3b55dc5d0d65f1c99aa1ae54d97fa
Vitor Mac <5874907+listanegra@users.noreply.github.com>
2022-07-23 21:33:57 -0300

    Vue3 + TailwindCSS project setup

---
+++ src/router.ts
@@ -0,0 +1,10 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import type { RouteRecordRaw } from 'vue-router';
+
+const routes: RouteRecordRaw[] = [
+];
+
+export const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
