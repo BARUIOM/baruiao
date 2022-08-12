76e73f5db656792903c454a13f4612057852a6fd
Vitor Mac <5874907+listanegra@users.noreply.github.com>
2022-08-12 03:43:07 -0300

    Player & Home layout

---
+++ src/components/Player.vue
@@ -0,0 +1,48 @@
+<script lang="ts" setup>
+import { Forms } from '@/components/forms';
+import { currentTime, currentTrack } from '@/modules/Player';
+</script>
+
+<template>
+    <div class="player" v-if="currentTrack">
+        <div>
+            <img :src="currentTrack.album.images[0].url" />
+            <div>
+                <div>{{ currentTrack.name }}</div>
+                <div class="text-sm text-white/40">{{ currentTrack.artists[0].name }}</div>
+            </div>
+        </div>
+        <div>
+            <Forms.Slider v-model="currentTime" :max="currentTrack.duration_ms / 1000"></Forms.Slider>
+        </div>
+        <div></div>
+    </div>
+</template>
+
+<style lang="postcss" scoped>
+div.player {
+    @apply flex;
+    @apply h-24 min-h-[6rem] p-4;
+    @apply border-t border-white/10;
+    @apply bg-[#181818];
+
+    >div:nth-child(1) {
+        @apply flex items-center space-x-2;
+        @apply w-[30%] h-full;
+
+        >img {
+            @apply h-[inherit];
+            @apply shadow;
+        }
+    }
+
+    >div:nth-child(2) {
+        @apply flex items-center;
+        @apply w-[40%];
+    }
+
+    >div:nth-child(3) {
+        @apply w-[30%];
+    }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,30 @@
+<script lang="ts" setup>
+import Player from '@/components/Player.vue';
+</script>
+
 <template>
-    <router-view name="header"></router-view>
-    <router-view></router-view>
+    <main>
+        <div class="header"></div>
+        <div class="content">
+            <router-view />
+        </div>
+        <Player></Player>
+    </main>
 </template>
+
+<style lang="postcss" scoped>
+main {
+    @apply flex flex-col;
+    @apply w-full h-full;
+
+    >div.header {
+        @apply h-16 min-h-[4rem];
+        @apply border-b border-white/10;
+        @apply bg-[#181818];
+    }
+
+    >div.content {
+        @apply h-full p-2;
+    }
+}
+</style>
