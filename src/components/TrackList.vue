59c3e30479794fb9592044593899a7e7166096f1
Vitor Mac <5874907+listanegra@users.noreply.github.com>
2022-08-15 13:30:12 -0300

    Limit option for the TrackList component

--- src/components/TrackList.vue
+++ src/components/TrackList.vue
@@ -1,24 +1,34 @@
 <script lang="ts" setup>
+import { computed, toRefs } from 'vue';
 import Player from '@/modules/Player';
 
 import TrackListItem from './TrackListItem.vue';
 
 type Props = {
+    limit?: number;
     tracks: SpotifyApi.TrackObjectFull[];
 };
 
-defineProps<Props>();
+const props = defineProps<Props>();
+const { limit, tracks } = toRefs(props);
 
 const play = (track: SpotifyApi.TrackObjectFull) => {
     Player.pause();
     return Player.load(track)
         .then(Player.play);
 };
+
+const trackList = computed(() => {
+    if (limit?.value)
+        return tracks.value.slice(0, limit.value);
+
+    return tracks.value;
+});
 </script>
 
 <template>
     <div class="p-2">
-        <template v-for="track in tracks" :key="track.id">
+        <template v-for="track in trackList" :key="track.id">
             <TrackListItem :track="track" @click.stop="play(track)"></TrackListItem>
         </template>
     </div>
